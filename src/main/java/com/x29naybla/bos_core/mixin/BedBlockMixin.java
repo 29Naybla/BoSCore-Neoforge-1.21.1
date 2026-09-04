15375a562f1c24b57fe8edc5c482dec817025af0
x29Naybla <29Naybla@gmail.com>
2026-09-03 22:36:03 -0300

    changed cake recipe, replaced vanilla sugar recipe with one made in the oven, bed voxel shape changed

--- src/main/java/com/x29naybla/bos_core/mixin/BedBlockMixin.java
+++ src/main/java/com/x29naybla/bos_core/mixin/BedBlockMixin.java
@@ -1,18 +1,41 @@
 package com.x29naybla.bos_core.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.*;
 
 @Mixin(value = BedBlock.class, priority = 2000)
 public abstract class BedBlockMixin extends Block {
+    @Shadow
+    @Final
+    public static BooleanProperty OCCUPIED;
+    @Unique
+    private static final VoxelShape BOS_SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 9.0, 16.0);
+    @Unique
+    private static final VoxelShape BOS_SHAPE_OCCUPIED = Block.box(0.0, 0.0, 0.0, 16.0, 14.0, 16.0);
+
     public BedBlockMixin(Properties properties) {
         super(properties);
+    }
+
+    /**
+     * @author 29Naybla
+     * @reason Changing the shape to be simpler and the appropriate size when it's occupied
+     */
+    @Overwrite
+    @Override
+    protected @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        if(state.getValue(OCCUPIED)) return BOS_SHAPE_OCCUPIED;
+        else return BOS_SHAPE;
     }
 
     /**
